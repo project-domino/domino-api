8fcdf8780b94fdd571ad03e3c2cadc69dccfeedf
Nathaniel Ringo <remexre@gmail.com>
2016-08-04 03:36:43 -0500

    Creates skeleton of final v1 API.

--- api/v1/init.go
+++ api/v1/init.go
@@ -8,6 +8,26 @@ import (
 
 func init() {
 	api.RegisterVersion("1.0.0-alpha", func(db *gorm.DB, r gin.IRouter) {
+		// Collection API
+		r.POST("/collection", TODO)
+		r.Group("/collection/:collection", LoadCollection(db, true)).
+			GET("/", Terminal("collection")).
+			POST("/", TODO)
+
+		// Note API
+		r.POST("/note", TODO)
+		r.Group("/note/:note", LoadNote(db, true)).
+			GET("/", Terminal("note")).
+			POST("/", TODO)
+
+		// TODO Search API
+		// Or should the search server be separate?
+
+		// Tags API
+		r.GET("/tags", TODO)
+		r.POST("/tags", TODO)
+
+		// User API
 		r.POST("/user/create",
 			LoginUser(db, false, "admin"),
 			CreateUser(db))
@@ -16,6 +36,8 @@ func init() {
 			CreateAuthToken(db))
 		r.Group("/user/:user", LoadUser(db)).
 			GET("/", Terminal("user")).
+			GET("/collections", TODO).
+			GET("/notes", TODO).
 			GET("/notifications", TODO)
 	})
 }
